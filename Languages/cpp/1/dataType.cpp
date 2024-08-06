f3a2e91522c193cb76fa98ff1ccc9c4eca13812f
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-08-06 22:30:32 +0530

    Adding Day 1 files

---
+++ Languages/cpp/1/dataType.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 23;
+    cout << a << endl;
+
+    char b = 's';
+    cout << b << endl;
+
+    bool c = true;
+    cout << c << endl;
+
+    float d = 1.23;
+    cout << d << endl;
+
+    double e = 1.45;
+    cout << e << endl;
+}
